214bc5b3312cc6c041d61f901e4c4ef5afa8ad5f
Yuri Mandrikov <mandrikov.dev@gmail.com>
2025-01-03 15:16:59 +0400

    refactor: refac PickManifestTask

--- src/tasks/pick-manifest-task.ts
+++ src/tasks/pick-manifest-task.ts
@@ -1,136 +1,102 @@
 import { ProjectExplorer } from "../project-explorer";
 import { Manifest } from "../manifest";
 import { loggable, Logger } from "../logger";
-import vscode, { QuickPickItem, QuickPickItemKind } from "vscode";
+import vscode, { CancellationError, CancellationToken, QuickPickItem, QuickPickItemKind } from "vscode";
 import { ExtState } from "../ext-state";
-import { ExtContext } from "../ext-context";
 
-function getDetail(manifest: Manifest): string | undefined {
-  return manifest.relativePath;
-}
-
-function getManifestsPicks(manifests: Manifest[], recentManifest: Manifest | undefined): Thenable<QuickPickItem[]> {
-  const list: Promise<QuickPickItem>[] = [];
-
-  const recentManifestInList = manifests.find((m) => m.fsPath === recentManifest?.fsPath);
-
-  if (recentManifestInList) {
-    list.push(
-      new Promise((resolve) => {
-        resolve({
-          label: "Last used",
-          kind: QuickPickItemKind.Separator,
-        });
-      }),
-    );
-
-    list.push(
-      new Promise((resolve) => {
-        resolve(
-          recentManifestInList.loadContent().then((m) => {
-            return {
-              label: m.displayName || m.name,
-              detail: getDetail(recentManifestInList),
-            };
-          }),
-        );
-      }),
-    );
-  }
-
-  const filtered = manifests.filter((m) => {
-    if (!recentManifestInList) {
-      return true;
-    }
-
-    return m.fsPath !== recentManifestInList.fsPath;
-  });
-
-  if (filtered.length === 0) {
-    return Promise.all(list);
-  }
-
-  if (recentManifestInList) {
-    list.push(
-      new Promise((resolve) => {
-        resolve({
-          label: "",
-          kind: QuickPickItemKind.Separator,
-        });
-      }),
-    );
-  }
-
-  filtered.forEach((m) => {
-    list.push(
-      new Promise((resolve) => {
-        resolve(
-          m.loadContent().then((manifest) => {
-            return {
-              label: manifest.displayName || manifest.name,
-              detail: getDetail(m),
-            };
-          }),
-        );
-      }),
-    );
-  });
-
-  return Promise.all(list);
+interface ManifestQuickPickItem extends QuickPickItem {
+  manifest?: Manifest;
 }
 
 export class PickManifestTask {
   constructor(
+    private readonly state: ExtState = ExtState.instance,
     private readonly pe: ProjectExplorer = new ProjectExplorer(),
     private readonly logger: Logger = Logger.getInstance(),
   ) {}
 
   @loggable()
-  async run(): Promise<Manifest | undefined> {
-    return new Promise(async (resolve) => {
-      try {
-        const state = new ExtState(ExtContext.context);
-        const manifests = await this.getAllManifests();
+  async run(token?: CancellationToken): Promise<Manifest | undefined> {
+    if (token?.isCancellationRequested) {
+      throw new CancellationError();
+    }
 
-        if (manifests.length === 0) {
-          this.logger.log("No manifest.yml found");
-          vscode.window.showErrorMessage("No manifest.yml found");
-          resolve(undefined);
-          return;
-        }
+    try {
+      const manifests = await this.getAllManifests();
 
-        const recent = state.getRecentManifest();
-
-        const selected = await vscode.window.showQuickPick(getManifestsPicks(manifests, recent), {
-          placeHolder: "Choose a manifest.yml file",
-          matchOnDetail: true,
-        });
-
-        if (!selected) {
-          resolve(undefined);
-          return;
-        }
-
-        const manifest = manifests.find((m) => m.relativePath === selected.detail);
-
-        resolve(manifest);
-      } catch (e) {
-        this.logger.error("Unhandled Error:", e);
-        resolve(undefined);
+      if (manifests.length === 0) {
+        this.handleNoManifests();
+        return;
       }
+
+      const quickPickItems = await this.createQuickPickItems(manifests);
+      const selected = await this.showQuickPick(quickPickItems);
+
+      if (!selected) {
+        return;
+      }
+
+      return manifests.find((m) => m.relativePath === selected.detail);
+    } catch (e) {
+      this.logger.error("Unhandled Error:", e);
+    }
+  }
+
+  private async getAllManifests(): Promise<Manifest[]> {
+    const manifests = await this.pe.findAllManifests();
+
+    return manifests.map((m) => {
+      this.logger.log(`Found manifest: ${m.path}`);
+      return new Manifest(m);
     });
   }
 
-  private async getAllManifests() {
-    return this.pe.findAllManifests().then((ms) => {
-      const manifests: Manifest[] = [];
+  private handleNoManifests() {
+    const message = "No manifest.yml found";
+    this.logger.log(message);
+    vscode.window.showErrorMessage(message);
+  }
 
-      ms.forEach((m) => {
-        this.logger.log(`Found manifest: ${m.path}`);
-        manifests.push(new Manifest(m));
+  private async createQuickPickItems(manifests: Manifest[]): Promise<ManifestQuickPickItem[]> {
+    const items: Array<Promise<ManifestQuickPickItem>> = [];
+    const persisted = this.state.getRecentManifest();
+    const recent = manifests.find((m) => m.path.path === persisted?.path?.path);
+
+    if (recent) {
+      items.push(this.createSeparatorItem("Last used"));
+      items.push(this.createManifestQuickPickItem(recent));
+      items.push(this.createSeparatorItem(""));
+    }
+
+    const other = manifests.filter((m) => m.path.path !== recent?.path?.path);
+    const otherItems = other.map((m) => this.createManifestQuickPickItem(m));
+    items.push(...otherItems);
+
+    return Promise.all(items);
+  }
+
+  private async showQuickPick(items: ManifestQuickPickItem[]): Promise<ManifestQuickPickItem | undefined> {
+    return vscode.window.showQuickPick(items, {
+      placeHolder: "Choose a manifest.yml file",
+      matchOnDetail: true,
+    });
+  }
+
+  private async createSeparatorItem(label: string): Promise<ManifestQuickPickItem> {
+    return {
+      label,
+      kind: QuickPickItemKind.Separator,
+    };
+  }
+
+  private async createManifestQuickPickItem(manifest: Manifest): Promise<ManifestQuickPickItem> {
+    return new Promise((resolve) => {
+      manifest.loadContent().then((m) => {
+        resolve({
+          label: m.displayName || m.name,
+          detail: m.relativePath,
+        });
       });
-
-      return manifests;
     });
   }
 }
