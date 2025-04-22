c217826870a5457987a4864c688d57f6a035854b
Yuri Mandrikov <mandrikov.dev@gmail.com>
2025-04-22 22:27:46 +0400

    refactor: remove unused command imports from extension.ts

--- src/commands/remove-recent-device-node.ts
+++
@@ -1,7 +0,0 @@
-import { ExtState } from "../ext-state";
-import { RecentDevicesTreeItem } from "../recent-devices-provider";
-
-export const removeRecentDeviceNode = (node: RecentDevicesTreeItem) => {
-  const state = ExtState.getInstance();
-  void state.removeRecentDevice(node.device);
-};
--- src/commands/select-recent-as-active-by-tree-node.ts
+++
@@ -1,7 +0,0 @@
-import { ExtState } from "../ext-state";
-import { RecentDevicesTreeItem } from "../recent-devices-provider";
-
-export const selectRecentAsActiveByTreeNode = (node: RecentDevicesTreeItem) => {
-  const state = ExtState.getInstance();
-  void state.setActiveDevice(node.device);
-};
