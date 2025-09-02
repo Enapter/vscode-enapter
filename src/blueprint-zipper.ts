4892802ff347566c82c8cac284bfaeaa481a8693
Yuri Mandrikov <mandrikov.dev@gmail.com>
2025-09-02 21:14:46 +0400

    refac: simplify BlueprintZipper

--- src/blueprint-zipper.ts
+++ src/blueprint-zipper.ts
@@ -1,83 +1,31 @@
 import vscode from "vscode";
 import { LoadedManifest } from "./models/manifests/manifest";
 import JSZip from "jszip";
-import { ILogger, loggable, Logger } from "./logger";
 
 export class BlueprintZipper {
   private manifest: LoadedManifest;
   private readonly zipper: JSZip;
+  private readonly permittedFilesGlob = "**/*.{lua,rockspec}";
 
-  constructor(
-    manifest: LoadedManifest,
-    private readonly logger: ILogger = Logger.getInstance(),
-  ) {
+  constructor(manifest: LoadedManifest) {
     this.manifest = manifest;
     this.zipper = new JSZip();
   }
 
-  @loggable()
   async zip() {
-    this.zipper.file(this.manifest.filename, this.manifest.contentStr);
+    const parentDirUri = vscode.Uri.joinPath(this.manifest.uri, "..");
+    const globStr = `${vscode.workspace.asRelativePath(parentDirUri)}/${this.permittedFilesGlob}`;
+    const uris = await vscode.workspace.findFiles(globStr);
 
-    if (await this.isLuaDir()) {
-      await this.zipLuaDir();
-    } else {
-      await this.zipLuaFile();
+    for (const uri of uris) {
+      const content = await vscode.workspace.fs.readFile(uri);
+      const relativePath = uri.path.replace(parentDirUri.path + "/", "");
+      this.zipper.file(relativePath, content, { createFolders: false });
     }
 
-    if (this.manifest.rockspec && this.manifest.rockspecPath) {
-      await this.zipRockspecFile(this.manifest.rockspecPath, this.manifest.rockspec);
-    }
+    const manifestContent = await vscode.workspace.fs.readFile(this.manifest.uri);
+    this.zipper.file("manifest.yml", manifestContent);
 
     return this.zipper.generateAsync({ type: "uint8array" });
-  }
-
-  private async zipLuaDir() {
-    const luaDirPath = this.getLuaFsPath();
-    await this.addFilesToZip(luaDirPath, this.zipper);
-  }
-
-  private async addFilesToZip(dirPath: string, zipFolder: JSZip) {
-    const dirUri = vscode.Uri.file(dirPath);
-    const files = await vscode.workspace.fs.readDirectory(dirUri);
-
-    for (const [name, type] of files) {
-      const filePath = `${dirPath}/${name}`;
-      const fileUri = vscode.Uri.file(filePath);
-
-      if (type === vscode.FileType.Directory) {
-        const newFolder = zipFolder.folder(name);
-        await this.addFilesToZip(filePath, newFolder!);
-      } else if (type === vscode.FileType.File) {
-        const fileContent = await vscode.workspace.fs.readFile(fileUri);
-        const relativePath = this.manifest.luaPath.replace(/\\/g, "/") + `/${name}`;
-        zipFolder.file(relativePath, fileContent, { createFolders: false });
-      }
-    }
-  }
-
-  private async zipLuaFile() {
-    const luaContent = await vscode.workspace.fs.readFile(vscode.Uri.file(this.getLuaFsPath()));
-    this.zipper.file(this.manifest.luaPath, luaContent);
-  }
-
-  private async zipRockspecFile(path: string, filename: string) {
-    const rockspecContent = await vscode.workspace.fs.readFile(vscode.Uri.file(path));
-    this.zipper.file(filename, rockspecContent);
-  }
-
-  private getLuaFsPath() {
-    const path = this.manifest.luaFsPath;
-    this.logger.log("Lua path:", path);
-    return this.manifest.luaFsPath;
-  }
-
-  private async isLuaDir() {
-    return this.isDir(this.getLuaFsPath());
-  }
-
-  private async isDir(path: string) {
-    const stat = await vscode.workspace.fs.stat(vscode.Uri.file(path));
-    return stat.type === vscode.FileType.Directory;
   }
 }
