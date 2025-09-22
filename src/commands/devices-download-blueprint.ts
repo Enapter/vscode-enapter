87edf102a30023acfcd423483b59dd1abc8a7854
Yuri Mandrikov <mandrikov.dev@gmail.com>
2025-09-22 18:32:24 +0400

    feat: add progress indicator and success notification for blueprint download

--- src/commands/devices-download-blueprint.ts
+++ src/commands/devices-download-blueprint.ts
@@ -1,8 +1,32 @@
 import { Device } from "../models/device";
 import { SitesConnectionsService } from "../services/sites-connections-service";
 import { ApiClient } from "../api/client";
-import vscode from "vscode";
+import vscode, { ProgressLocation } from "vscode";
 import JSZip from "jszip";
+import { getFilename } from "../utils/get-filename";
+
+const withProgress = (cb: Parameters<typeof vscode.window.withProgress>[1]) => {
+  return vscode.window.withProgress(
+    {
+      location: ProgressLocation.Notification,
+      title: "Downloading Blueprint",
+      cancellable: true,
+    },
+    cb,
+  );
+};
+
+class RevealInExplorerMessageItem implements vscode.MessageItem {
+  constructor(private uri: vscode.Uri) {}
+
+  get title() {
+    return "Reveal in the Explorer";
+  }
+
+  reveal() {
+    vscode.commands.executeCommand("revealInExplorer", this.uri);
+  }
+}
 
 export const devicesDownloadBlueprint = async (device: Device, sitesConnectionsServices: SitesConnectionsService) => {
   const activeSite = sitesConnectionsServices.getActive();
@@ -40,25 +64,38 @@ export const devicesDownloadBlueprint = async (device: Device, sitesConnectionsS
     return;
   }
 
-  const blob = await apiClient.downloadBlueprintAsZipByDeviceId(blueprintId);
+  return withProgress(async () => {
+    const blob = await apiClient.downloadBlueprintAsZipByDeviceId(blueprintId);
 
-  if (shouldUnzip) {
-    const zip = await new JSZip().loadAsync(await blob.arrayBuffer());
+    if (shouldUnzip) {
+      const zip = await new JSZip().loadAsync(await blob.arrayBuffer());
 
-    for (const filename of Object.keys(zip.files)) {
-      const file = zip.files[filename];
-      const writeUri = vscode.Uri.joinPath(vscode.Uri.file(path), filename);
+      for (const filename of Object.keys(zip.files)) {
+        const file = zip.files[filename];
+        const writeUri = vscode.Uri.joinPath(vscode.Uri.file(path), filename);
 
-      if (!file.dir) {
-        const content = await file.async("uint8array");
-        await vscode.workspace.fs.writeFile(writeUri, content);
-      } else {
-        await vscode.workspace.fs.createDirectory(writeUri);
+        if (!file.dir) {
+          const content = await file.async("uint8array");
+          await vscode.workspace.fs.writeFile(writeUri, content);
+        } else {
+          await vscode.workspace.fs.createDirectory(writeUri);
+        }
       }
+    } else {
+      await vscode.workspace.fs.writeFile(vscode.Uri.file(path), await blob.bytes());
     }
-  } else {
-    await vscode.workspace.fs.writeFile(vscode.Uri.file(path), await blob.bytes());
-  }
 
-  return blob;
+    vscode.window
+      .showInformationMessage(
+        `Blueprint for the device ${device.name} downloaded successfully`,
+        new RevealInExplorerMessageItem(vscode.Uri.file(path)),
+      )
+      .then((revealInExplorerMessageItem) => {
+        if (!revealInExplorerMessageItem) {
+          return;
+        }
+
+        revealInExplorerMessageItem.reveal();
+      });
+  });
 };
